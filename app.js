5dc89d518582622005caba4b04d11f4037fd8bd8
congchu <cookie00421@gmail.com>
2022-01-29 16:34:18 +0900

    [fix] 포트번호 변경

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var express = require('express')
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express()
 
-// 3000 포트로 서버 오픈
-app.listen(3000, function () {
+// 5000 포트로 서버 오픈
+app.listen(5000, function () {
     console.log("start! express server on port 3000")
 })
 
 // request 와 response 라는 인자를 줘서 콜백 함수를 만든다.
-// localhost:3000 브라우저에 res.send() 내부의 문자열이 띄워진다.
+// localhost:5000 브라우저에 res.send() 내부의 문자열이 띄워진다.
 
 app.get('/', function (req, res) {
     res.send("<h1>hi friend!</h1>")
