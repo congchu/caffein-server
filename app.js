b5d2d5b827c49f98926c6e2d89124697881ca030
congchu <cookie00421@gmail.com>
2022-01-29 16:43:22 +0900

    [fix] 포트 환경 변수에서 가져오도록

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 
+const PORT = process.env.PORT || 5000
+
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express()
 
 // 5000 포트로 서버 오픈
-app.listen(5000, function () {
+app.listen(PORT, function () {
     console.log("start! express server on port 3000")
 })
 
